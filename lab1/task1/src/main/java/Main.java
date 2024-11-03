aefc827eedbfb68ac2670d38872f25f5210d7be5
George Răbuș <george.rabus@isa.utm.md>
2024-11-03 14:13:48 +0200

    Learn how objects can interact with other objects, through a comparison method in the class.

--- lab1/task1/src/main/java/Main.java
+++ lab1/task1/src/main/java/Main.java
@@ -8,14 +8,19 @@ class Car {
         this.model = model;
         this.year = year;
     }
+
+    boolean isSameMake(Car otherCar) {
+        return this.make.equals(otherCar.make);
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         Car car1 = new Car("Toyota", "Camry", 2022);
-        Car car2 = new Car("Honda", "Civic", 2023);
+        Car car2 = new Car("Toyota", "Corolla", 2023);
+        Car car3 = new Car("Honda", "Civic", 2023);
 
-        System.out.println(car1.make + " " + car1.model + " " + car1.year);
-        System.out.println(car2.make + " " + car2.model + " " + car2.year);
+        System.out.println(car1.isSameMake(car2)); // true
+        System.out.println(car1.isSameMake(car3)); // false
     }
 }
