ae49868235d829697b38a004a51a9a37b29143b4
George Răbuș <george.rabus@isa.utm.md>
2024-11-13 13:38:12 +0200

    task 1

--- lab2/src/main/java/Main.java
+++ lab2/src/main/java/Main.java
@@ -1,3 +1,100 @@
+enum Intensity {
+    LIGHT, NORMAL, STRONG
+}
+
+enum SyrupType {
+    MACADAMIA, VANILLA, COCONUT, CARAMEL, CHOCOLATE, POPCORN
+}
+
+class Coffee {
+    protected Intensity coffeeIntensity;
+    protected final String name = "Coffee";
+
+    public Coffee(Intensity coffeeIntensity) {
+        this.coffeeIntensity = coffeeIntensity;
+    }
+
+    public Intensity getCoffeeIntensity() {
+        return coffeeIntensity;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Americano extends Coffee {
+    private int mlOfWater;
+    private final String coffeeName = "Americano";
+
+    public Americano(Intensity intensity, int mlOfWater) {
+        super(intensity);
+        this.mlOfWater = mlOfWater;
+    }
+
+    public int getMlOfWater() {
+        return mlOfWater;
+    }
+
+    public String getCoffeeName() {
+        return coffeeName;
+    }
+}
+
+class Cappuccino extends Americano {
+    private int mlOfMilk;
+    private final String coffee = "Cappuccino";
+
+    public Cappuccino(Intensity intensity, int mlOfWater, int mlOfMilk) {
+        super(intensity, mlOfWater);
+        this.mlOfMilk = mlOfMilk;
+    }
+
+    public int getMlOfMilk() {
+        return mlOfMilk;
+    }
+
+    public String getCoffee() {
+        return coffee;
+    }
+}
+
+class SyrupCappuccino extends Cappuccino {
+    private SyrupType syrup;
+    private final String coffee = "SyrupCappuccino";
+
+    public SyrupCappuccino(Intensity intensity, int mlOfWater, int mlOfMilk, SyrupType syrup) {
+        super(intensity, mlOfWater, mlOfMilk);
+        this.syrup = syrup;
+    }
+
+    public SyrupType getSyrup() {
+        return syrup;
+    }
+
+    public String getCoffee() {
+        return coffee;
+    }
+}
+
+class PumpkinSpiceLatte extends SyrupCappuccino {
+    private int mgOfPumpkinSpice;
+    private final String name = "PumpkinSpiceLatte";
+
+    public PumpkinSpiceLatte(Intensity intensity, int mlOfWater, int mlOfMilk, SyrupType syrup, int mgOfPumpkinSpice) {
+        super(intensity, mlOfWater, mlOfMilk, syrup);
+        this.mgOfPumpkinSpice = mgOfPumpkinSpice;
+    }
+
+    public int getMgOfPumpkinSpice() {
+        return mgOfPumpkinSpice;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
 public class Main {
 
 }
