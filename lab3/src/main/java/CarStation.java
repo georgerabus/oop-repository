56b6e7570e8e99f331303a95b02ac02c042d851c
George Răbuș <george.rabus@isa.utm.md>
2024-12-16 23:21:28 +0200

    small changes to testObj to get new attributes

--- lab3/src/main/java/CarStation.java
+++ lab3/src/main/java/CarStation.java
@@ -1,5 +1,4 @@
 import java.util.List;
-
 public class CarStation {
     public static void main(String[] args){
         String priorityFuel = "Gas";
@@ -7,18 +6,21 @@ public class CarStation {
 
         Queue<testObj> queue = new PriorityQueue<>(7, new ObjectComparator(priorityFuel, priorityDining));
         List<testObj> numbers = List.of(
-                new testObj("Gas", "People"),
-                new testObj("Electric", "Robots"),
-                new testObj("Electric", "Robots"),
-                new testObj("Electric", "People"),
-                new testObj("Gas", "People"),
-                new testObj("Gas", "Robots"),
-                new testObj("Gas", "People")
+                new testObj(1,"Gas", "People", "yes"),
+                new testObj(2,"Electric", "Robots", "no"),
+                new testObj(3, "Electric", "Robots", "yes"),
+                new testObj(4,"Electric", "People","no" ),
+                new testObj(5, "Gas", "People", "no"),
+                new testObj(6, "Gas", "Robots", "no"),
+                new testObj(7,"Gas", "People", "yes")
                 );
 
         for (testObj number : numbers) {
             queue.enqueue(number);
         }
+
+        GasStation station = new GasStation();
+        station.refuel("1");
 
         while (!queue.isEmpty()) {
             System.out.println(queue.dequeue());
--- lab3/src/main/java/testObj.java
+++ lab3/src/main/java/testObj.java
@@ -2,10 +2,14 @@ public class testObj {
 
     private String fuel;
     private String dining;
+    private int id;
+    private String does_dine;
 
-    public testObj(String dining, String fuel) {
+    public testObj(int id, String dining, String fuel, String does_dine) {
         this.dining = dining;
         this.fuel = fuel;
+        this.id = id;
+        this.does_dine = does_dine;
     }
 
     public String getDining() {
@@ -16,8 +20,12 @@ public class testObj {
         return fuel;
     }
 
+    public int getId() {return id;}
+
+    public String getDoes_dine() {return does_dine;}
+
     @Override
     public String toString() {
-        return "testObj {dining = '" + dining + "', fuel ='" + fuel + "'}";
+        return "testObj {id = " + id + ", dining = '" + dining + "', fuel = '" + fuel + "', wants to dine = '" + does_dine + "'}";
     }
 }
